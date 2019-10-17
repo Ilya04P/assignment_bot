85cd9fe65463adcee20694a0c1fb2c08a0ac7f29
Ilya Kalugin <kalugin.ilya@gmail.com>
2019-10-17 21:15:37 +0300

    add get text message

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from telegram import ReplyKeyboardMarkup
 
 
@@ -6,3 +8,17 @@ def get_keyboard():
         ['Заметка']
     ], resize_keyboard=True)
     return general_keyboard
+
+
+def get_note_message(note):
+    note_id = note['note_id']
+    caption = note['caption']
+    text = note['text']
+    d_modify = datetime.timestamp(note['d_modify'])
+    underline = ''
+
+    for e in len(caption):
+        underline = underline + '-'
+
+    text_message = '#{}\n{}\n{}\n{}\n--\n{}'.format(note_id, caption, underline, text, d_modify)
+
